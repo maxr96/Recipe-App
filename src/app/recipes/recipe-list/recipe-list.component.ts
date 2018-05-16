af102411143ec1cd8863a9f993f937535604a7a0
Max <maxr96@gmail.com>
2018-05-16 22:46:37 +0200

    Moved from bootstrap 4 to 3. Added recipe item.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [];
   constructor() { }
 
   ngOnInit() {
