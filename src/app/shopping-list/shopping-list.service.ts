acfdd3c7bcb791986379317cd774b75641d4cb32
Maksym Radchuk <maxr96@gmail.com>
2020-09-25 21:26:07 +0200

    Fix logic for adding new items in a shopping list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable({providedIn: "root"})
 export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
@@ -12,5 +13,6 @@ export class ShoppingListService {
       }
       addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
       }
 }
