ec65f0afb0ce1f2e1c5eef5fd46e9fc8e33df595
Max <max@pop-os.localdomain>
2020-10-11 16:39:57 +0200

    Add reactive forms for editing and adding new recipes

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable({providedIn: 'root'})
 export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 
         'This is simply a test', 
@@ -35,5 +37,15 @@ export class RecipeService {
 
       getRecipe(id: number) {
           return this.recipes[id];
+      }
+
+      addRecipe(recipe: Recipe){
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+      }
+
+      updateRecipe(index: number, newRecipe: Recipe){
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
       }
 }
