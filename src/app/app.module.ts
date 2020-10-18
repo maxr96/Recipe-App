33fd073d569141cb8bb376f4414df88ffeb1a84e
Maksym Radchuk <maxr96@gmail.com>
2020-10-18 12:02:39 +0200

    Use lazy loading with preload strategy aall modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -22,7 +21,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     CoreModule,
--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -7,15 +7,30 @@ import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipesResolverService } from "./recipes-resolver.service";
 import { RecipesComponent } from "./recipes.component";
 
-const routes: Routes = [  
-    { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard],
-children: [
-   { path: '', component: RecipeStartComponent },
-   { path: 'new', component: RecipeEditComponent },
-   { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-   { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] }
-] },
-];
+const routes: Routes = [{
+  path: '',
+  component: RecipesComponent,
+  canActivate: [AuthGuard],
+  children: [{
+      path: '',
+      component: RecipeStartComponent
+    },
+    {
+      path: 'new',
+      component: RecipeEditComponent
+    },
+    {
+      path: ':id',
+      component: RecipeDetailComponent,
+      resolve: [RecipesResolverService]
+    },
+    {
+      path: ':id/edit',
+      component: RecipeEditComponent,
+      resolve: [RecipesResolverService]
+    }
+  ]
+}, ];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
