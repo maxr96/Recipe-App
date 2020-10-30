648781cbb3c4382000e2188469d34c12e516aec4
Maksym Radchuk <maxr96@gmail.com>
2020-10-30 18:41:14 +0100

    :art: Materialize signup and login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,15 +4,24 @@ import { RouterModule } from "@angular/router";
 import { SharedModule } from "../shared/shared.module";
 import { LoginComponent } from "./login.component";
 import { SignupComponent } from "./signup.component";
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
   imports: [FormsModule,
     MatFormFieldModule,
     MatButtonModule,
+    FlexLayoutModule,
+    MatInputModule,
     RouterModule.forChild([{
+      path: '',
+      redirectTo: 'login',
+      pathMatch: 'full'
+    },{
       path: 'login',
       component: LoginComponent
     },{
