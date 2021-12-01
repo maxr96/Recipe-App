1184588d7e3ec457c0408f2461aac74319795cb3
Max <maxr96@gmail.com>
2021-12-01 11:52:25 +0100

    :zap: Use dynamic imports to lazy load routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,28 @@
-import { NgModule } from "@angular/core";
-import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { importType } from '@angular/compiler/src/output/output_ast';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
-    { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'},
-    { path: 'auth', loadChildren: './auth/auth.module#AuthModule'}
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module').then((m) => m.RecipesModule),
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () =>
+      import('./shopping-list/shopping-list.module').then((s) => s.ShoppingListModule),
+  },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then((a) => a.AuthModule) },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes, {
+      preloadingStrategy: PreloadAllModules,
+      relativeLinkResolution: 'legacy',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
